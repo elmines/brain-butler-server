b2a137b737b954f6e02f1b93b1201dc58f28e7ec
Ethan Mines <elmines325@gmail.com>
2019-03-20 10:13:04 -0500

    Send new arithmetic problems to phone

--- src/flow/ws.js
+++ src/flow/ws.js
@@ -1,4 +1,5 @@
 //@flow
+const fs = require("fs");
 const WebSocket = require("ws");
 const PacketManager = require("./packetManager.js");
 import type EventEmitter from "events";
@@ -8,6 +9,13 @@ const log = require("./logging.js").log;
 
 const man: PacketManager = new PacketManager();
 
+var problemIndex: number = 0;
+var problems: Array<Object>;
+fs.readFile("./problems.json", function(err, data) {
+  const text: string = data.toString()
+  problems = JSON.parse(text);
+});
+
 function wsServer(port: number = 8080) {
   const wss = new WebSocket.Server({port});
 
@@ -16,22 +24,29 @@ function wsServer(port: number = 8080) {
     emitter.on("submission", (packet: Object): void => {
       man.process(packet);
 
-      const event: string = packet.name;
-      //const action = (event == "strategy") ? "displayIntertrial": "displayStrategyPrompt";
+      let   sendProblem: boolean = false;
+      const       event: string  = packet.name;
 
       let action: string;
       switch(event) {
         case "startExperiment":
           action = "startExperiment";
+          sendProblem = true;
           break;
         case "strategy":
           action = "displayIntertrial";
+          sendProblem = true;
           break;
         default:
           action = "displayStrategyPrompt";
       }
-
+      if (sendProblem) {
+          const problem: Object = problems[problemIndex].text;
+          ws.send(JSON.stringify({problem}));
+          problemIndex = (problemIndex + 1) % problems.length;
+      }
       ws.send(JSON.stringify({action}));
+
     });
 
     ws.on("message", function incoming(message: string){
