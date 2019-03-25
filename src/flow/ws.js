012ace0a2acbe34aad1158ee5d7e538be622c994
Ethan Mines <elmines325@gmail.com>
2019-03-24 20:35:44 -0500

    Don't send messages through closed sockets

--- src/flow/ws.js
+++ src/flow/ws.js
@@ -2,9 +2,8 @@
 const fs = require("fs");
 const WebSocket = require("ws");
 const PacketManager = require("./packetManager.js");
-import type EventEmitter from "events";
 
-const emitter: EventEmitter = require("./emitter.js");
+const emitter = require("./emitter.js");
 const log = require("./logging.js").log;
 
 const man: PacketManager = new PacketManager();
@@ -16,14 +15,23 @@ fs.readFile("./problems.json", function(err, data) {
   problems = JSON.parse(text);
 });
 
+
 function wsServer(port: number = 8080) {
   const wss = new WebSocket.Server({port});
+  wss.on("connection", (ws) => {
+    problemIndex = 0;
+    emitter.on("submission", packet => reply(ws, packet));
 
-  wss.on("connection", function connection(ws){
+    ws.on("message", (message: string) => {
+      const parsed: Object = JSON.parse(message);
+      man.process(parsed);
+    });
+  });
+}
 
-    emitter.on("submission", (packet: Object): void => {
+
+function reply(ws, packet) {
       man.process(packet);
-
       let   sendProblem: boolean = false;
       const       event: string  = packet.name;
 
@@ -42,19 +50,15 @@ function wsServer(port: number = 8080) {
       }
       if (sendProblem) {
           const problem: Object = problems[problemIndex].text;
-          ws.send(JSON.stringify({problem}));
+          safeSend(ws, JSON.stringify({problem}));
           problemIndex = (problemIndex + 1) % problems.length;
       }
-      ws.send(JSON.stringify({action}));
+      safeSend(ws, JSON.stringify({action}));
+}
 
-    });
-
-    ws.on("message", function incoming(message: string){
-      const parsed: Object = JSON.parse(message);
-      man.process(parsed);
-    });
-  });
-  return wss;
+function safeSend(ws, message) {
+    if (ws.readyState != WebSocket.OPEN) return;
+    ws.send(message);
 }
 
 module.exports = wsServer;
